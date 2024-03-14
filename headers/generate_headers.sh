8d1ecb506bdc4db295a4bf95abf1810399765511
jl <jlima@nubificus.co.uk>
2024-03-14 05:36:48 +0000

    add headers

---
+++ headers/generate_headers.sh
@@ -0,0 +1,10 @@
+python ../nanopb/generator/nanopb_generator.py -I ../proto error.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto genop.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto image.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto profiling.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto resources.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto session.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto tensorflow.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto torch.proto
+python ../nanopb/generator/nanopb_generator.py -I ../proto agent.proto
+

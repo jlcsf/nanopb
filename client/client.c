14499cdacfe9441c48e7fe78e4b968ee3a6a9367
jl <jlima@nubificus.co.uk>
2024-03-15 05:17:18 +0000

    add cmake

--- client/client.c
+++ client/client.c
@@ -6,6 +6,7 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #include "../nanopb/pb_encode.h"
 #include <../nanopb/pb_decode.h>
@@ -22,12 +23,16 @@
 #include "../headers/tensorflow.pb.h"
 #include "../headers/torch.pb.h"
 
+#define PORT 12345
+#define REQUEST_BUFFER_SIZE 8192
+
 int main(int argc, char **argv)
 {
     vaccel_VaccelRequest request = vaccel_VaccelRequest_init_zero;
     request.function_type = vaccel_VaccelFunctionType_CREATE_SESSION;
 
     vaccel_CreateSessionRequest createSessionRequest = vaccel_CreateSessionRequest_init_zero;
+    createSessionRequest.flags = 0;
 
     uint8_t createSessionRequestBuffer[vaccel_CreateSessionRequest_size];
     pb_ostream_t createSessionRequestStream = pb_ostream_from_buffer(createSessionRequestBuffer, sizeof(createSessionRequestBuffer));
@@ -36,6 +41,36 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    size_t message_length = createSessionRequestStream.bytes_written;
     request.function_request.arg = &createSessionRequestStream;
     request.function_request.funcs.encode = (bool (*)(pb_ostream_t *, const pb_field_t *, void * const*)) &pb_encode_string;
+
+
+    /// connect to server
+
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+    }
+
+    uint8_t request_buffer[REQUEST_BUFFER_SIZE]; 
+    size_t request_length = message_length;
+
+    if (send(client_socket, request_buffer, request_length, 0) == -1) {
+        perror("Send failed");
+    }
+
+    close(client_socket);
+
+    return 0;
 }
